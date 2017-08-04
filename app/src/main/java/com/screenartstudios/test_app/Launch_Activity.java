db8bdd8e0f322e4fae4a4557801c9a06a61767df
Durban-Designer <royce.birnbaum@gmail.com>
2017-08-04 11:50:53 -0700

    Empty App Template Added
    Added in the base template.

---
+++ app/src/main/java/com/screenartstudios/test_app/Launch_Activity.java
@@ -0,0 +1,13 @@
+package com.screenartstudios.test_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Launch_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.launch_activity_layout);
+    }
+}
